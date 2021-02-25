dd2e2d76bc5670bf3d91cb66e468f42569e44056
Vincent Delphin <vincentmledelphin@gmail.com>
2021-02-25 12:59:17 +0000

    Added error handling, returns error for incorrect crendentials and if you have already signed in.

--- Driver.py
+++ Driver.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementNotInteractableException
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome()
@@ -10,13 +12,24 @@ username = f.readline()
 password = f.readline()
 f.close()
 
-print(username, password)
 
-username_box = driver.find_element_by_id('userNameInput')
-username_box.send_keys(username)
+try:
+    username_box = driver.find_element_by_id('userNameInput')
+    username_box.send_keys(username)
 
-password_box = driver.find_element_by_id('passwordInput')
-password_box.send_keys(password)
+    password_box = driver.find_element_by_id('passwordInput')
+    password_box.send_keys(password)
 
-login_button = driver.find_element_by_id('submitButton')
-login_button.click()
+    login_button = driver.find_element_by_id('submitButton')
+    login_button.click()
+    try:
+        register_attendance_online_button = driver.find_element_by_id('pbid-buttonFoundHappeningNowButtonsTwoOnline')
+        register_attendance_online_button.click()
+
+    except ElementNotInteractableException as exception:
+        print("You are already signed in!")
+
+except NoSuchElementException as exception:
+    print("Credentials given in credentials.txt are incorrect.")
+
+
